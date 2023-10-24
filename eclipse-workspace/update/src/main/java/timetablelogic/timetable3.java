1def2ce635e5e437ac66d046e591a765637645af
ryuk7 <ryuk7@Pranays_Laptop>
2023-10-25 00:16:10 +0530

    Increased width

--- eclipse-workspace/update/src/main/java/timetablelogic/timetable3.java
+++ eclipse-workspace/update/src/main/java/timetablelogic/timetable3.java
@@ -96,7 +96,7 @@ public class timetable3 extends HttpServlet {
 				        + "    padding: 20px;"
 				        + "    border-radius: 10px;"
 				        + "    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);"
-				        + "    width: 400px;"
+				        + "    width: 600px;"
 				        + "    text-align: center;"
 				        + "}"
 				        + "p {"
