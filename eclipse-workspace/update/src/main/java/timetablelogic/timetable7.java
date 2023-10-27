c4d1aaa26b4d67cc0928ffc283f5413f6933aa7c
ryuk7 <ryuk7@Pranays_Laptop>
2023-10-27 11:08:58 +0530

    Partially improved the design of the final timetable generation page.

--- eclipse-workspace/update/src/main/java/timetablelogic/timetable7.java
+++ eclipse-workspace/update/src/main/java/timetablelogic/timetable7.java
@@ -29,65 +29,109 @@ public class timetable7 extends HttpServlet {
 		int l35=0;
 		System.out.println("We're in 7");
 		System.out.println(allcomb.size());
-		String htmlContent = "<html>"
-                  + "<head>"
-                  + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
-                  + "<title>Possible Timetable Combinations:</title>"
-                  + "</head>"
-                  + "<body>"
-                  + "<h1>Possible Timetable Combinations:</h1>";
-
+		String htmlContent = "<!DOCTYPE html>"
+                + "<html>"
+                + "<head>"
+                + "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+                + "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Raleway\">"
+                + "    <style>"
+                + "        body {"
+                + "            margin: 0;"
+                + "            padding: 0;"
+                + "            font-family: 'Raleway', sans-serif;"
+                + "            background: #34495E;"
+                + "            color: #fff;"
+                + "            display: flex;"
+                + "            justify-content: center;"
+                + "            align-items: center;"
+                + "            min-height: 100vh;"
+                + "        }"
+                + "        .container {"
+                + "            background: rgba(0, 0, 0, 0.6);"
+                + "            padding: 20px;"
+                + "            border-radius: 10px;"
+                + "            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);"
+                + "            width: 600px;"
+                + "            text-align: center;"
+                + "        }"
+                + "        p {"
+                + "            font-size: 18px;"
+                + "            margin-bottom: 10px;"
+                + "        }"
+                + "        a {"
+                + "            text-decoration: none;"
+                + "        }"
+                + "        button {"
+                + "            padding: 10px 20px;"
+                + "            font-size: 18px;"
+                + "            font-family: 'Raleway', sans-serif;"
+                + "            background-color: #2ecc71;"
+                + "            border: none;"
+                + "            border-radius: 5px;"
+                + "            color: #fff;"
+                + "            cursor: pointer;"
+                + "            transition: background-color 0.3s ease, transform 0.3s ease;"
+                + "            margin: 10px;"
+                + "        }"
+                + "        button:hover {"
+                + "            background-color: #27ae60;"
+                + "            transform: scale(1.1);"
+                + "        }"
+                + "    </style>"
+                + "    <title>Possible Timetable Combinations:</title>"
+                + "</head>"
+                + "<body>"
+                + "    <div class=\"container\">"
+                + "        <h1>Possible Timetable Combinations:</h1>";
 
 for (Brute ob1 : allcomb) {
-l35++;
-htmlContent += "<h2>Timetable #" + l35 +":"+ "</h2>";
-// Details for pure theory subjects
-htmlContent += "<div><h2>Details for pure theory subjects:</h2><ul>";
-for (Map.Entry<String, HashMap<String, HashSet<String>>> get : ob1.theory.entrySet()) {
-    htmlContent += "<li><strong>" + get.getKey() + "</strong>:<ul>";
-    for (Map.Entry<String, HashSet<String>> get1 : get.getValue().entrySet()) {
-        htmlContent += "<li>" + removeNumbers(get1.getKey()) + " : " + get1.getValue() + "</li>";
+    l35++;
+    htmlContent += "<h2>Timetable #" + l35 + ":" + "</h2>";
+    
+    // Details for pure theory subjects
+    htmlContent += "<div><h2>Details for pure theory subjects:</h2>";
+    for (Map.Entry<String, HashMap<String, HashSet<String>>> get : ob1.theory.entrySet()) {
+        htmlContent += "<p><strong>" + get.getKey() + "</strong>:";
+        for (Map.Entry<String, HashSet<String>> get1 : get.getValue().entrySet()) {
+            htmlContent += "<p>" + removeNumbers(get1.getKey()) + " : " + get1.getValue() + "</p>";
+        }
     }
-    htmlContent += "</ul></li>";
-}
-htmlContent += "</ul></div>";
+    htmlContent += "</div>";
 
-// Details for pure lab subjects
-htmlContent += "<div><h2>Details for pure lab subjects:</h2><ul>";
-for (Map.Entry<String, HashMap<String, HashSet<String>>> entry : ob1.lab.entrySet()) {
-    htmlContent += "<li><strong>" + entry.getKey() + "</strong>:<ul>";
-    for (Map.Entry<String, HashSet<String>> innerEntry : entry.getValue().entrySet()) {
-        htmlContent += "<li>" + removeNumbers(innerEntry.getKey()) + " : " + innerEntry.getValue() + "</li>";
+    // Details for pure lab subjects
+    htmlContent += "<div><h2>Details for pure lab subjects:</h2>";
+    for (Map.Entry<String, HashMap<String, HashSet<String>>> entry : ob1.lab.entrySet()) {
+        htmlContent += "<p><strong>" + entry.getKey() + "</strong>:";
+        for (Map.Entry<String, HashSet<String>> innerEntry : entry.getValue().entrySet()) {
+            htmlContent += "<p>" + removeNumbers(innerEntry.getKey()) + " : " + innerEntry.getValue() + "</p>";
+        }
     }
-    htmlContent += "</ul></li>";
-}
-htmlContent += "</ul></div>";
+    htmlContent += "</div>";
 
-// Details for embedded subjects
-htmlContent += "<div><h2>Details for embedded subjects:</h2><ul>";
-for (Map.Entry<String, HashMap<String, List<List<String>>>> subjectEntry : ob1.embedded.entrySet()) {
-    htmlContent += "<li><strong>" + subjectEntry.getKey() + "</strong>:<ul>";
-    for (Map.Entry<String, List<List<String>>> teacherEntry : subjectEntry.getValue().entrySet()) {
-        htmlContent += "<li><strong>" + removeNumbers(teacherEntry.getKey()) + "</strong>:<ul>";
-        htmlContent += "<li>Theory slots: " + teacherEntry.getValue().get(0) + "</li>";
-        htmlContent += "<li>Lab slots: " + teacherEntry.getValue().get(1) + "</li>";
-        htmlContent += "</ul></li>";
+    // Details for embedded subjects
+    htmlContent += "<div><h2>Details for embedded subjects:</h2>";
+    for (Map.Entry<String, HashMap<String, List<List<String>>>> subjectEntry : ob1.embedded.entrySet()) {
+        htmlContent += "<p><strong>" + subjectEntry.getKey() + "</strong>:";
+        for (Map.Entry<String, List<List<String>>> teacherEntry : subjectEntry.getValue().entrySet()) {
+            htmlContent += "<p><strong>" + removeNumbers(teacherEntry.getKey()) + "</strong>:";
+            htmlContent += "<p>Theory slots: " + teacherEntry.getValue().get(0) + "</p>";
+            htmlContent += "<p>Lab slots: " + teacherEntry.getValue().get(1) + "</p>";
+        }
     }
-    htmlContent += "</ul></li>";
-}
-htmlContent += "</ul></div>";
+    htmlContent += "</div>";
 
-htmlContent += "<hr>";
-if(l35==100) {
-	break;
-}
+    htmlContent += "<hr>";
+    if (l35 == 100) {
+        break;
+    }
 }
 
-htmlContent += "</body>"
-            + "</html>";
+htmlContent += "</div>" + "</body>"
+        + "</html>";
 
-//Set the response content type to HTML
+// Set the response content type to HTML
 res.setContentType("text/html");
+
 
 //Get the PrintWriter to write the HTML content to the response
 PrintWriter out = res.getWriter();
