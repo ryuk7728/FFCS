9f3c1f7bd21e97dd1016bd11d70be6a050447c0c
ryuk7 <ryuk7@Pranays_Laptop>
2023-10-27 21:03:18 +0530

    Finished the design of the final timetable views page

--- eclipse-workspace/update/src/main/java/timetablelogic/timetable7.java
+++ eclipse-workspace/update/src/main/java/timetablelogic/timetable7.java
@@ -30,92 +30,118 @@ public class timetable7 extends HttpServlet {
 		System.out.println("We're in 7");
 		System.out.println(allcomb.size());
 		String htmlContent = "<!DOCTYPE html>"
-                + "<html>"
-                + "<head>"
-                + "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
-                + "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Raleway\">"
-                + "    <style>"
-                + "        body {"
-                + "            margin: 0;"
-                + "            padding: 0;"
-                + "            font-family: 'Raleway', sans-serif;"
-                + "            background: #34495E;"
-                + "            color: #fff;"
-                + "            display: flex;"
-                + "            justify-content: center;"
-                + "            align-items: center;"
-                + "            min-height: 100vh;"
-                + "        }"
-                + "        .container {"
-                + "            background: rgba(0, 0, 0, 0.6);"
-                + "            padding: 20px;"
-                + "            border-radius: 10px;"
-                + "            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);"
-                + "            width: 600px;"
-                + "            text-align: center;"
-                + "        }"
-                + "        p {"
-                + "            font-size: 18px;"
-                + "            margin-bottom: 10px;"
-                + "        }"
-                + "        a {"
-                + "            text-decoration: none;"
-                + "        }"
-                + "        button {"
-                + "            padding: 10px 20px;"
-                + "            font-size: 18px;"
-                + "            font-family: 'Raleway', sans-serif;"
-                + "            background-color: #2ecc71;"
-                + "            border: none;"
-                + "            border-radius: 5px;"
-                + "            color: #fff;"
-                + "            cursor: pointer;"
-                + "            transition: background-color 0.3s ease, transform 0.3s ease;"
-                + "            margin: 10px;"
-                + "        }"
-                + "        button:hover {"
-                + "            background-color: #27ae60;"
-                + "            transform: scale(1.1);"
-                + "        }"
-                + "    </style>"
-                + "    <title>Possible Timetable Combinations:</title>"
-                + "</head>"
-                + "<body>"
-                + "    <div class=\"container\">"
-                + "        <h1>Possible Timetable Combinations:</h1>";
+		        + "<html>"
+		        + "<head>"
+		        + "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+		        + "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Raleway\">"
+		        + "    <style>"
+		        + "        body {"
+		        + "            margin: 0;"
+		        + "            padding: 0;"
+		        + "            font-family: 'Raleway', sans-serif;"
+		        + "            background: #34495E;"
+		        + "            color: #fff;"
+		        + "            display: flex;"
+		        + "            justify-content: center;"
+		        + "            align-items: center;"
+		        + "            min-height: 100vh;"
+		        + "        }"
+		        + "        .container {"
+		        + "            background: rgba(0, 0, 0, 0.6);"
+		        + "            padding: 20px;"
+		        + "            border-radius: 10px;"
+		        + "            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5);"
+		        + "            width: 600px;"
+		        + "            text-align: center;"
+		        + "        }"
+		        + "        h1 {"
+		        + "            font-size: 32px;"
+		        + "            margin-bottom: 50px; /* Increase padding below the title */"
+		        + "            text-align: center;"
+		        + "        }"
+		        + "        h2 {"
+		        + "            font-size: 24px; /* Separate styling for h2 */"
+		        + "            margin-bottom: 20px; /* Separate margin for h2 */"
+		        + "            text-align: left; /* Left-align the h2 tag */"
+		        + "        }"
+		        + "        p {"
+		        + "            font-size: 18px;"
+		        + "            margin-bottom: 10px;"
+		        + "            text-align: left; /* Left-align the p tag */"
+		        + "        }"
+		        + "        a {"
+		        + "            text-decoration: none;"
+		        + "        }"
+		        + "        button {"
+		        + "            padding: 10px 20px;"
+		        + "            font-size: 18px;"
+		        + "            font-family: 'Raleway', sans-serif;"
+		        + "            background-color: #2ecc71;"
+		        + "            border: none;"
+		        + "            border-radius: 5px;"
+		        + "            color: #fff;"
+		        + "            cursor: pointer;"
+		        + "            transition: background-color 0.3s ease, transform 0.3s ease;"
+		        + "            margin: 10px;"
+		        + "        }"
+		        + "        button:hover {"
+		        + "            background-color: #27ae60;"
+		        + "            transform: scale(1.1);"
+		        + "        }"
+		        + "    </style>"
+		        + "    <title>Possible Timetable Combinations:</title>"
+		        + "</head>"
+		        + "<body>"
+		        + "    <div class=\"container\">"
+		        + "        <h1>Possible Timetable Combinations:</h1>";
 
 for (Brute ob1 : allcomb) {
     l35++;
     htmlContent += "<h2>Timetable #" + l35 + ":" + "</h2>";
     
     // Details for pure theory subjects
+    if(ob1.theory.size()!=0) {
     htmlContent += "<div><h2>Details for pure theory subjects:</h2>";
+    }
+    else {
+    	htmlContent += "<div>";
+    }
     for (Map.Entry<String, HashMap<String, HashSet<String>>> get : ob1.theory.entrySet()) {
         htmlContent += "<p><strong>" + get.getKey() + "</strong>:";
         for (Map.Entry<String, HashSet<String>> get1 : get.getValue().entrySet()) {
-            htmlContent += "<p>" + removeNumbers(get1.getKey()) + " : " + get1.getValue() + "</p>";
+            htmlContent += "<p>" + removeNumbers(get1.getKey()) + ": " +get1.getValue() + "</p>";
         }
     }
     htmlContent += "</div>";
 
     // Details for pure lab subjects
+    if(ob1.lab.size()!=0) {
     htmlContent += "<div><h2>Details for pure lab subjects:</h2>";
+    }
+    else {
+    	htmlContent += "<div>";
+    }
     for (Map.Entry<String, HashMap<String, HashSet<String>>> entry : ob1.lab.entrySet()) {
         htmlContent += "<p><strong>" + entry.getKey() + "</strong>:";
         for (Map.Entry<String, HashSet<String>> innerEntry : entry.getValue().entrySet()) {
-            htmlContent += "<p>" + removeNumbers(innerEntry.getKey()) + " : " + innerEntry.getValue() + "</p>";
+            htmlContent += "<p>" + removeNumbers(innerEntry.getKey()) + ": " + innerEntry.getValue() + "</p>";
         }
     }
     htmlContent += "</div>";
 
     // Details for embedded subjects
+    if(ob1.embedded.size()!=0) {
     htmlContent += "<div><h2>Details for embedded subjects:</h2>";
+    }
+    else {
+    	htmlContent += "<div>";
+    }
     for (Map.Entry<String, HashMap<String, List<List<String>>>> subjectEntry : ob1.embedded.entrySet()) {
         htmlContent += "<p><strong>" + subjectEntry.getKey() + "</strong>:";
         for (Map.Entry<String, List<List<String>>> teacherEntry : subjectEntry.getValue().entrySet()) {
-            htmlContent += "<p><strong>" + removeNumbers(teacherEntry.getKey()) + "</strong>:";
-            htmlContent += "<p>Theory slots: " + teacherEntry.getValue().get(0) + "</p>";
-            htmlContent += "<p>Lab slots: " + teacherEntry.getValue().get(1) + "</p>";
+            htmlContent += "<p>" + removeNumbers(teacherEntry.getKey())+": ";
+            htmlContent +=teacherEntry.getValue().get(0)+" ";
+            htmlContent += teacherEntry.getValue().get(1);
         }
     }
     htmlContent += "</div>";
